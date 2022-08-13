7cdc6dc97e6bd2ffc974c5bf11d93e362227ba65
crypticq <85430867+crypticq@users.noreply.github.com>
2022-08-13 16:11:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 import json
 import time
 import logging
+import random
 
 
-headers = {
 
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0",
-}
+
+
+headers = {"Accept": "*/*", "Accept-Language": "en-US,en;q=0.9", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/604.18 FABUILD/7.3.0 FABUILD-iOS/7.3.0 APP/7.3.0", "Referer": "https://www.pacsun.com/mens/graphic-tees/?country=SA&FABUILD=iosmobileapp&start=24", "Accept-Encoding": "gzip, deflate"}
+
+print(headers)
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
@@ -19,24 +22,29 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 s = requests.Session()
-s.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0'})
+
 
 url = "https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/default/Search-ShowAjax?cgid=mens-graphic-tees&start=1&sz=1&country=SA"
 
-how_many = BeautifulSoup(s.get(url).content , 'lxml').find('p' , {'class':'item-search-p'})
+how_many = BeautifulSoup(s.get(url , headers=headers).content , 'lxml').find('p' , {'class':'item-search-p'})
 loop =  int(how_many.text.split()[0].strip('('))
-print(f'Total products found {loop}')
-time.sleep(10)
+print(f'Total t-shrits found {loop}')
+
+def dol_to_sar(amount):
+	return int(amount.split()[0].split('.')[0].strip('$')) * 3.75
+
 u = 0
 result = []
 while u <= loop:
-    u+= 20
+    u+= 100
     try:
-        response = s.get(f'https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/default/Search-ShowAjax?cgid=mens-graphic-tees&start={u}&sz=20&country=SA', headers=headers)
+        response = s.get(f'https://www.pacsun.com/on/demandware.store/Sites-pacsun-Site/'
+                         'default/Search-ShowAjax?cgid=mens-graphic-tees'
+                         f'&start={u}&sz=100&country=SA' , headers=headers)
 
         logger.info(f' Visiting {response.url} with status code {response.status_code} ')
         print('Sleeping just to bypass cloudFlare <--')
-        time.sleep(15)
+        time.sleep(4)
         soup = BeautifulSoup(response.content , 'html.parser')
 
 
@@ -46,10 +54,11 @@ while u <= loop:
             product_name = soup.findAll('a' , class_='link')[i].text.strip()
             product_brand = soup.findAll('div' , class_='text-product-brand flex-row align-items-center')[i].text.strip()
             product_image = soup.findAll('img' , {'class':'w-100'})[i]['src'].split('?')[0]
-            price = (soup.findAll('span' , class_='value bfx-price bfx-sale-price')[i].text.strip())
-            result.append({'name':product_name , 'brand':product_brand , 'price':price , 'img':product_image})
+            price = dol_to_sar(soup.findAll('span' , class_='value bfx-price bfx-sale-price')[i].text.strip())
+            link =  "https://www.pacsun.com" + soup.findAll('div' , class_="product-tile")[i].find('a').get('href')
+            result.append({'name':product_name , 'brand':product_brand , 'price':price , 'img':product_image , 'link':link} )
     except Exception as e:
-        logger.info(e)
+        logger.error(e)
         pass
     
     
